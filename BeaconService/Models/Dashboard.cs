7ffe76e77b19f9dee1578d8c2b47655d2fc601f2
Nick Wilson <thenickwilson@gmail.com>
2016-04-15 16:53:41 +0100

    Added Dashboard model

---
+++ BeaconService/Models/Dashboard.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace BeaconService.Models
+{
+	public class Dashboard
+	{
+		public List<SightingDocument> Sightings { get; set; }
+		public string ErrorMessage { get; set; }
+	}
+}
